74f54c0d21e370a2d6f47d12201f34bc2108f49f
Sergey Belyaev <79952005+sbelka-1703@users.noreply.github.com>
2022-11-13 09:41:21 -0700

    Gray color tests are passing

--- src/App.test.js
+++ src/App.test.js
@@ -41,3 +41,29 @@ test('when checkbox is checked, button should be disabled', () => {
   expect(colorButton).toBeEnabled();
 
 })
+
+test('button turns gray when it is disabled', () => {
+  render(<App/>)
+
+  const colorButton = screen.getByRole('button', {name: 'Change to blue'})
+
+  const checkboxForColorButton = screen.getByRole('checkbox' ,{name: 'Disable button'})
+
+  fireEvent.click(checkboxForColorButton);
+
+  expect(colorButton).toHaveStyle({background: 'gray'})
+
+  fireEvent.click(checkboxForColorButton);
+
+  expect(colorButton).toHaveStyle({background: 'red'})
+
+  fireEvent.click(colorButton)
+
+  fireEvent.click(checkboxForColorButton)
+
+  expect(colorButton).toHaveStyle({background: 'gray'})
+
+  fireEvent.click(checkboxForColorButton)
+
+  expect(colorButton).toHaveStyle({background: 'blue'})
+})
